203ae04fec779e372087134779bb94312670a216
Allan Jones <allanpauljones@gmail.com>
2017-04-11 19:09:24 +1000

    Add 'wo' alias

--- code/projects.zsh
+++ code/projects.zsh
@@ -1,2 +1,8 @@
 alias cz='c && cd Czar'
 alias ser='c && cd Serenity'
+
+# wo == 'work on': https://coderwall.com/p/feoi0a/shell-cd-replacement
+wo() {
+	dir=$(find $PROJECTS -maxdepth 3 -type d | grep -i $* | grep -Ev Pods --max-count=1)
+	[[ -d $dir ]] && cd $dir
+}
