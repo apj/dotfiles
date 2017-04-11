f7ac8b34cfdac8a7a031cb30f92d2648a4676f8c
Allan Jones <allanpauljones@gmail.com>
2017-04-11 19:09:55 +1000

    CONFIG: Update git settings

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -15,7 +15,7 @@ alias gp='git push origin HEAD'
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
-alias gca='git commit -a'
+alias gca='git commit --amend'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
