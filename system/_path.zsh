3fb0f35a5512c5637a4665dbb82e973c8a8732bb
Allan Jones <allanpauljones@gmail.com>
2018-01-28 00:15:37 +1100

    Add python3 to PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+export PATH=$PATH:~/Library/Python/3.6/bin
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
